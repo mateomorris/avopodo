6d30dc0165c88e2f2f286229835ad62a70bdbc17
Mateo Morris <mateo@insidenewcity.com>
2018-09-08 14:39:34 -0400

    Subscribed Screen: bottom padding when playbar active

--- src/screens/SubscribedScreen.js
+++ src/screens/SubscribedScreen.js
@@ -53,75 +53,14 @@ class SubscribedScreen extends React.Component {
   render() {
 
     this.state = {
-      favorites: [
-        {
-          title: 'This American Life',
-          art: 'https://media.npr.org/images/podcasts/primary/icon_381444650-04c1bad8586e69edf04b78ea319846614c4a6a6b-s800-c15.png',
-        },
-        {
-          title: 'Planet Money',
-          art: 'https://secureimg.stitcher.com/feedimagesplain328/7668.jpg',
-        },
-        {
-          title: 'Pod Save America',
-          art: 'https://secureimg.stitcher.com/feedimagesplain328/127661.jpg',
-        }
-      ],
-
-      playlists: [
-        {
-          title: 'All NPR',
-          duration: '5 Hours',
-          icon: require('../assets/radio.png'),
-          episodes: [
-            {
-              art: 'https://media.npr.org/images/podcasts/primary/icon_381444650-04c1bad8586e69edf04b78ea319846614c4a6a6b-s800-c15.png'
-            },
-            {
-              art: 'https://secureimg.stitcher.com/feedimagesplain328/127661.jpg'
-            },
-            {
-              art: 'https://secureimg.stitcher.com/feedimagesplain328/7668.jpg'
-            }
-          ]
-        },
-        {
-          title: 'Trending',
-          duration: '3 Hours',
-          icon: require('../assets/trending.png'),
-          episodes: [
-            {
-              art: 'https://secureimg.stitcher.com/feedimagesplain328/7668.jpg'
-            },
-            {
-              art: 'https://secureimg.stitcher.com/feedimagesplain328/127661.jpg'
-            },
-            {
-              art: 'https://media.npr.org/images/podcasts/primary/icon_381444650-04c1bad8586e69edf04b78ea319846614c4a6a6b-s800-c15.png'
-            }
-          ]
-        }
-      ],
-
-      homeFeed: [
-        {
-          title: 'Telling the Truth',
-          art: 'https://secureimg.stitcher.com/feedimagesplain328/127661.jpg',
-          duration: '32m',
-          description: 'Every time it rains, Marisa stays up all night wondering if anyone will show up to work the next day. She runs a bike messenger company, but not one wants to ride a bike when it’s pouring...'
-        },
-        {
-          title: 'Gerrymandering',
-          art: 'https://secureimg.stitcher.com/feedimagesplain328/7668.jpg',
-          duration: '53m',
-          description: `The way we draw our political districts has a huge effect on U.S. politics, but the process is also greatly misunderstood. Gerrymandering has become a scapegoat for what's wrong with the...`
-        }
-      ]
+      favorites: [],
+      playlists: [],
+      homeFeed: []
     };
     
     return (
       <View style={{ flex: 1, backgroundColor: '#fafafa' }}>
-        <ScrollView contentContainerStyle={styles.container}>
+        <ScrollView contentContainerStyle={[styles.container, { paddingBottom: this.props.state.active ? 50 : 5 } ]}>
           { this._renderFavorites(this.props.state.subscribedShows) }
         </ScrollView>
       </View>
