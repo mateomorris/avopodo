517fe175444e5780ddb8bf8b09ebbffdbd8ba61b
Mateo Morris <mateo@insidenewcity.com>
2019-01-05 13:16:30 -0500

    Bug fix: Some episodes weren't playing (added to exception domains)

--- src/redux/actions/middleware/getEpisodeListForShow.js
+++ src/redux/actions/middleware/getEpisodeListForShow.js
@@ -12,55 +12,59 @@ export function getEpisodeListForShow(showId, ageLimit = 0, nextEpisodeDate = ne
 
             let showColor = getState().subscribedShows.find((subscribedShow) => {
                 return subscribedShow.id == show.id
-            }).color
+            })
 
-            // Cut the list off at the point where an episode pub_date_ms is smaller than episodeRange        
-            let episodeList = show.episodes.filter((episode) => {
-                return episode.audio ? episode : null // ensure audio exists
-            }).map((episode) => {
-                return {
-                    id: episode.id,
-                    title: episode.title,
-                    description: episode.description.replace(/(<([^>]+)>)/ig,""), // strip html
-                    duration: episode.audio_length, 
-                    publishDate: episode.pub_date_ms,
-                    audio: episode.audio.replace('http://', 'https://'), // Doesn't load if the url isn't safe
-                    showId: show.id,
-                    showTitle: show.title,
-                    showImage: show.image,
-                    showDescription : show.description, 
-                    showPublisher : show.publisher,
-                    showWebsite: show.website,
-                    showColor
-                };
-            }).filter((episode) => {
-                return episode.publishDate > ageLimit ? episode : null
-            });
+            if (show.episodes) {
+                // Cut the list off at the point where an episode pub_date_ms is smaller than episodeRange        
+                let episodeList = show.episodes.filter((episode) => {
+                    return episode.audio ? episode : null // ensure audio exists
+                }).map((episode) => {
+                    return {
+                        id: episode.id,
+                        title: episode.title,
+                        description: episode.description.replace(/(<([^>]+)>)/ig,""), // strip html
+                        duration: episode.audio_length, 
+                        publishDate: episode.pub_date_ms,
+                        audio: episode.audio.replace('http://', 'https://'), // Doesn't load if the url isn't safe
+                        showId: show.id,
+                        showTitle: show.title,
+                        showImage: show.image,
+                        showDescription : show.description, 
+                        showPublisher : show.publisher,
+                        showWebsite: show.website,
+                        showColor : showColor.color ? showColor.color : '#222'
+                    };
+                }).filter((episode) => {
+                    return episode.publishDate > ageLimit ? episode : null
+                });
 
-            // // If last episode is within release range, keep fetching until the limit is reached (or no more episodes are left)
-            // // Works, but too API-heavy to implement right now
-            // if (episodeList.length == 10 && episodeList[9].publishDate > ageLimit) {
-            //     console.log(showId, ageLimit, episodeList[9].publishDate)
-            //     dispatch(getEpisodeListForShow(showId, ageLimit, episodeList[9].publishDate)).then((episodes) => {
-            //         episodeList = [...episodeList, ...episodes] 
-            //         console.log(episodeList)
-            //     })
-            // } else {
-            //     return episodeList;
-            // }
+                // // If last episode is within release range, keep fetching until the limit is reached (or no more episodes are left)
+                // // Works, but too API-heavy to implement right now
+                // if (episodeList.length == 10 && episodeList[9].publishDate > ageLimit) {
+                //     console.log(showId, ageLimit, episodeList[9].publishDate)
+                //     dispatch(getEpisodeListForShow(showId, ageLimit, episodeList[9].publishDate)).then((episodes) => {
+                //         episodeList = [...episodeList, ...episodes] 
+                //         console.log(episodeList)
+                //     })
+                // } else {
+                //     return episodeList;
+                // }
 
-            // If last episode is within release range, too bad
-            if (episodeList.length == 10 && episodeList[9].publishDate > ageLimit) {
-                return {
-                    complete: false,
-                    episodeList
-                };
-            } else {
-                return {
-                    complete: true,
-                    episodeList
-                };
+                // If last episode is within release range, too bad
+                if (episodeList.length == 10 && episodeList[9].publishDate > ageLimit) {
+                    return {
+                        complete: false,
+                        episodeList
+                    };
+                } else {
+                    return {
+                        complete: true,
+                        episodeList
+                    };
+                }
             }
+
+
 
           })
           .catch((error) =>{
